9d2d2f4c55625c90972ad1c62a2f0ef9234e70a0
Chen <2385005155@qq.com>
2025-03-07 16:57:49 +0800

    comment-system

--- src/site.config.ts
+++ src/site.config.ts
@@ -127,7 +127,7 @@ export const integ: IntegrationUserConfig = {
   waline: {
     enable: true,
     // Server service link
-    server: 'https://astro-theme-pure-waline.arthals.ink/',
+    server: 'https://waline-for-astro-theme-pure.vercel.app/',
     // Refer https://waline.js.org/en/guide/features/emoji.html
     emoji: ['bmoji', 'weibo'],
     // Refer https://waline.js.org/en/reference/client/props.html
